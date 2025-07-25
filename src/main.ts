6bfa35a04df03c439f7f4eb3dfae6621fc49e87b
Lukasz Gruszka <lukasz>
2025-07-25 15:15:21 +0200

    initial structure

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1>Hello, Vite!</h1>
+    <p class="read-the-docs">
+      This is a webpage built with TypeScript, HTML, and CSS. 🚀
+    </p>
+  </div>
+`
+
