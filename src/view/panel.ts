0cccaf66eda073878ef44228abecaa1e72e22b60
lukaszg84 <22541451+lukaszg84@users.noreply.github.com>
2025-08-03 21:32:51 +0000

    Fix height and simplify observers code.

--- src/view/panel.ts
+++ src/view/panel.ts
@@ -3,41 +3,11 @@ export class PanelView {
   readonly panelContainer = document.getElementById('panel') as HTMLDivElement;
   readonly settingsButton = document.getElementById('panel-settings-button') as HTMLButtonElement;
   readonly panelGoogle = document.getElementById('panel-google') as HTMLDivElement;
-  readonly styleObserver: MutationObserver;
-  // readonly googleSearch = [...document.body.children].find(el => el instanceof HTMLTableElement);;
-
   googleSearch: HTMLTableElement | null = null;
 
-
   constructor(onSettingsButtonClick: () => void) {
-    this.styleObserver = new MutationObserver(this.styleMutationObserver.bind(this));
     this.settingsButton.addEventListener('click', onSettingsButtonClick);
-
-    // This observer watches for the autocomplete element to be added to the page.
-    const creationObserver = new MutationObserver((_, observer) => {
-      const googleSearch = [...document.body.children].find(el => el instanceof HTMLTableElement);
-
-      if (googleSearch) {
-        console.log('Google autocomplete container found.');
-
-        // Google code is only modifying `top` property - let's calculate the bottom property
-        // once.
-        const containerRect = this.panelContainer.getBoundingClientRect();
-        const googleRect = this.panelGoogle?.getBoundingClientRect();
-        // container is at the bottom - it's height minus diff between top values
-        googleSearch.style.setProperty(
-          'bottom',
-          `${containerRect.height - (googleRect ? (googleRect.top - containerRect.top) : 0) + 5}px`);
-
-        // Now let's set the style observer to detect css changes.
-        this.googleSearch = googleSearch;
-        this.styleObserver.observe(this.googleSearch, { attributes: true, attributeFilter: ['style'] });
-        observer.disconnect();
-      }
-    });
-
-    // Start observing the entire document for added elements.
-    creationObserver.observe(document.body, { childList: true, subtree: true });
+    this.createObservers();
   }
 
   public init(): Promise<any> {
@@ -49,12 +19,44 @@ export class PanelView {
     this.panelContainer.parentElement?.removeChild(this.panelContainer);
   }
 
+  private createObservers() {
+    // This observer watches for style changes. It will be initialised after the Google
+    // autocomplete element is added.
+    const styleObserver = new MutationObserver(this.styleMutationObserver.bind(this));
+
+    // This observer watches for the autocomplete element to be added to the page.
+    const creationObserver = new MutationObserver((_, observer) => {
+      const googleSearch = [...document.body.children].find(el => el instanceof HTMLTableElement);
+
+      if (googleSearch) {
+        console.log('Google autocomplete container found.');
+
+        // Google code is only modifying `top` property - let's calculate the bottom property
+        // once.
+        const panelGoogleRect = this.panelGoogle.getBoundingClientRect();
+        // container is at the bottom - it's height minus diff between top values
+        googleSearch.style.setProperty(
+          'bottom',
+          // height of panel + padding + some extra space
+          `${panelGoogleRect.height + 30}px`);
+
+        // Now let's set the style observer to detect css changes.
+        this.googleSearch = googleSearch;
+        styleObserver.observe(this.googleSearch, { attributes: true, attributeFilter: ['style'] });
+        observer.disconnect();
+      }
+    });
+
+    // Start observing the entire document for added elements.
+    creationObserver.observe(document.body, { childList: true, subtree: true });
+  }
+
   private styleMutationObserver(_: MutationRecord[]) {
     // Ignoring MutationRecords as this is all about styles.
     // Checking if top is set as our modification will also trigger
     // observer.
     if (this.googleSearch?.style.top) {
-      this.googleSearch?.style.removeProperty('top');
+      this.googleSearch.style.removeProperty('top');
     }
   }
 }
